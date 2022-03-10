fe1c396655041be7e51f4a86c36855eeca1d55bc
Md. Shahin Bashar <46506236+sbiitju@users.noreply.github.com>
2022-03-11 00:48:14 +0600

    Initial commit

---
+++ app/src/main/java/com/sbiitju/defenseproject/view/ui/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.sbiitju.defenseproject.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sbiitju.defenseproject.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
---
+++ app/src/main/java/com/sbiitju/defenseproject/view/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.sbiitju.defenseproject.view.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sbiitju.defenseproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startActivity(Intent(this,AuthenticationActivity::class.java))
+        finish()
+    }
+}
