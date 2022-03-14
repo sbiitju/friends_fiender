e10f7a526563f113322cfa81483690c14346d0ce
Md. Shahin Bashar <shahin.bashar@brainstation-23.com>
2022-03-15 02:36:17 +0600

    Athentication Completed with BasedClass and SIgnOut

--- app/src/main/java/com/sbiitju/defenseproject/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/sbiitju/defenseproject/viewmodel/AuthViewModel.kt
@@ -2,18 +2,20 @@ package com.sbiitju.defenseproject.viewmodel
 
 import android.util.Log
 import android.view.View
+import androidx.core.app.ActivityCompat.startActivityForResult
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.sbiitju.defenseproject.R
 import com.sbiitju.defenseproject.util.ResponseInterface
 
 class AuthViewModel: ViewModel() {
     var responseInterface:ResponseInterface?=null
     fun signIn(view: View){
         Log.d("shahin","shahin")
-        var gso=GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
-        gso.requestEmail().build()
-        responseInterface?.onUpdate(gso)
-
+        // Configure Google Sign In
+        val googleSignInClient:GoogleSignInClient?=null
+        responseInterface?.onUpdate("check")
     }
 }
